24c23085f8628b9e0ded516f0489729bb97a3249
Spencer Cook <spencercook@Spencers-MacBook-Pro.local>
2018-07-05 17:24:43 -0400

    Should be working

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+// variables
+var racquet = 0;
+
 function start() {
-	alert('Hello world!');
+	document.getElementById("intro").style.visibility = "hidden";  
+}
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function getRacquet(num) {
+	alert(num);
 }
