84feeb27fa29266f87203f323286f4af16aeae9c
Spencer Cook <spencercook@Spencers-MacBook-Pro.local>
2018-07-06 00:00:36 -0400

    Scoring

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ var power = 0;
 var control = 0;
 var feel = 0;
 var touch = 0;
+var comfort = 0;
+var longevity = 0;
+
+// strings
+
 
 function start() {
 	document.getElementById("intro").style.visibility = "hidden";  
@@ -61,39 +66,56 @@ function getRacquet(num) {
 function styleOfPlay(style) {
 	switch (style) {
 		case 0: // agressive, offensive player
-		break;
+			power = power + 60;
+			break;
 		case 1: // consistent player with long rallies
-		break;
+			spin = spin + 80;
+			control = control + 60;
+			break;
 		case 2: // all around player
 		break;
 		case 3: // not sure about TYPE OF PLAYER
 		break;
 		case 4: // hits ball too long
-		break;
+			// needs control
+			control = control + 100;
+			// spin as well
+			spin = spin + 60;
+			break;
 		case 5: // not deep enough
+			// needs spin
+			spin = spin + 100;
 		break;
 		case 6: // can't hit targets
+			// needs control
+			control = control + 100;
 		break;
 		case 7: // not sure about downfall
 		break;
 		case 8: // arm uncomfortable
+			// needs comfort
+			comfort = comfort + 100;
 		break;
 		case 9: // arm comfortable
+			comfort = comfort + 10;
 		break;
 		case 10: // unsure about arm
 		break;
 		case 11: // breaks strings a lot
-		break;
+			longevity = longevity + 100;
+			break;
 		case 12: // does not break strings
-		break;
+			longevity = longevity + 10;
+			break;
 		case 13: // unsure about strings
 		break;
 		case 14: // volleys a lot
-		// needs touch
-		touch = touch + 100;
-		break;
+			// needs touch
+			touch = touch + 100;
+			break;
 		case 15: // does not volley a lot
-		break;
+			touch = touch + 10;
+			break;
 		case 16: // unsure about volleying
 		break;
 		default:
