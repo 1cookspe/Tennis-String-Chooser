d92f8ad118867d65633aedf71e3e36fdf979c5af
Spencer Cook <spencercook@Spencers-MacBook-Pro.local>
2018-07-05 20:10:57 -0400

    Adding cases for selections

--- script.js
+++ script.js
@@ -59,7 +59,33 @@ function getRacquet(num) {
 
 function styleOfPlay(style) {
 	switch (style) {
-		case 0:
+		case 0: // agressive, offensive player
+		break;
+		case 1: // consistent player with long rallies
+		break;
+		case 2: // all around player
+		break;
+		case 3: // not sure about TYPE OF PLAYER
+		break;
+		case 4: // hits ball too long
+		break;
+		case 5: // not deep enough
+		break;
+		case 6: // can't hit targets
+		break;
+		case 7: // not sure about downfall
+		break;
+		case 8: // arm uncomfortable
+		break;
+		case 9: // arm comfortable
+		break;
+		case 10: // unsure about arm
+		break;
+		case 11: // breaks strings a lot
+		break;
+		case 12: // does not break strings
+		break;
+		case 13: // unsure about strings
 		break;
 		default:
 		break;
